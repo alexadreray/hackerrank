af62dd17369cfe8f04d6150a5ac1098f098a3301
Alexandre <alexandre.silva_SERVICE@abril.com.br>
2016-03-10 17:17:17 -0300

    correções de lógica para funcionar em todos os casos

--- algorithms/pangrams.php
+++ algorithms/pangrams.php
@@ -1,26 +1,25 @@
 <?php
 /*
-Desenvolvedor: Alexandre Silva
+Developer: Alexandre Silva
 Input string for test: We promptly judged antique ivory buckles for the next prize    
-This sentence is a pangram
+Contest: https://www.hackerrank.com/challenges/pangrams
 */
 
-$_fp = fopen("php://stdin", "r");
-/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-$myString = fgets($_fp);
-$letras = range('a','z');
-$letras_maiusculas = range('A','Z');
-$espaco_vazio = array(" ");
-$flag = 0;
-//print_r($letras);
+//$_fp = fopen("php://stdin", "r");
+//$myString = fgets($_fp);
 
-for ($i=0; $i<strlen($myString); $i++){
-    //echo $myString[$i];
-	if (in_array($myString[$i],$letras) or in_array($myString[$i],$letras_maiusculas) or in_array($myString[$i],$espaco_vazio)){ ; }
-	else{ $flag+=1; }
+$myString = "We promptly judged antique ivory buckles for the next prize";
+$myString = str_replace(' ','',strtolower($myString)); //all lower and without spaces
+$letras = range('a','z');
+$flag = 0;
+$arr = str_split($myString);
+
+for ($i=0; $i<sizeof($letras); $i++){
+	if (in_array($letras[$i],$arr)){ ; }
+	else { $flag+=1; }
 }
 
 if ($flag==0) echo "pangram"; else echo "not pangram";
 
-fclose($myfile);
+//fclose($myfile);
 ?>
